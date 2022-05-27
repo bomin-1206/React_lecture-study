addf01270a597b29cc950c8c4d08091ba89b2a2f
bomin-1206 <gbm4655@naver.com>
2022-05-27 20:52:31 +0900

    React_lecture-0526

--- react-lecture-blog/src/App.js
+++ react-lecture-blog/src/App.js
@@ -1,15 +1,55 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
   let post = "강남 우동 맛집";
+  // let [글제목1] = useState("남자코트 추천");
+  // let [글제목2] = useState("강남 우동맛집");
+  // let [글제목3] = useState("파이썬독학");
+  let [글제목] = useState(["남자코트 추천", "강남 우동맛집", "파이썬독학"]);
+  let [좋아요, 좋아요변경] = useState(0);
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4>블로그임</h4>
+        <h4>ReactBlog</h4>
       </div>
-      <h4>{post}</h4>
+      {/* <div className="list">
+        <h4>{글제목1}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목2}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목3}</h4>
+        <p>2월 17일 발행</p>
+      </div> */}
+      <div className="list">
+        <h4>
+          {" "}
+          {글제목[0]}{" "}
+          <span
+            onClick={() => {
+              좋아요변경(좋아요 + 1);
+            }}
+          >
+            ❤️
+          </span>{" "}
+          {좋아요}
+        </h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[2]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
