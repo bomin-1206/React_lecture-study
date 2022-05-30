798f6561a683c643f93a7238e91a178b4617a4ea
bomin-1206 <gbm4655@naver.com>
2022-05-30 21:58:55 +0900

    React-0530

--- react-lecture-blog/src/App.js
+++ react-lecture-blog/src/App.js
@@ -7,8 +7,9 @@ function App() {
   // let [글제목1] = useState("남자코트 추천");
   // let [글제목2] = useState("강남 우동맛집");
   // let [글제목3] = useState("파이썬독학");
-  let [글제목] = useState(["남자코트 추천", "강남 우동맛집", "파이썬독학"]);
+  let [글제목, 글제목변경] = useState(["남자코트 추천", "강남 우동맛집", "파이썬독학"]);
   let [좋아요, 좋아요변경] = useState(0);
+  let [modal, setModal] = useState(false);
 
   return (
     <div className="App">
@@ -27,19 +28,32 @@ function App() {
         <h4>{글제목3}</h4>
         <p>2월 17일 발행</p>
       </div> */}
+      {/* 
+      <button onClick={() => {글제목변경(['여자코트 추천', '강남 우동맛집', '파이썬독학'])}}>이름 바꾸기</button>
+      <button onClick={() => {
+        let copy = [...글제목];
+        copy[0] ='여자코트 추천';
+        글제목변경(copy);
+        }}>글수정</button>
+        */}
+        <button onClick = {() => {
+          let copy = [...글제목];
+          copy[0] = '여자코트 추천';
+          글제목변경(copy);
+          // 실행 X
+          // let arr = [1,2,3];
+          // 글제목[0] ='여자코트 추천'; // array를 수정했지 변수에 있던 화살표는 수정안됨
+          // 글제목변경(글제목);
+          }}>글수정</button>
+
+          <button onClick = {() => {
+            let copy=[...글제목];
+            copy.sort();
+            글제목변경(copy);
+          }}>가나다순 정렬</button>
+
       <div className="list">
-        <h4>
-          {" "}
-          {글제목[0]}{" "}
-          <span
-            onClick={() => {
-              좋아요변경(좋아요 + 1);
-            }}
-          >
-            ❤️
-          </span>{" "}
-          {좋아요}
-        </h4>
+        <h4 onClick = {() => { setModal(!modal)}}> {글제목[0]} <span onClick={() => { 좋아요변경(좋아요 + 1) }}>❤️</span>{좋아요}</h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
@@ -50,8 +64,23 @@ function App() {
         <h4>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
+
+      {
+        modal == true ? <Modal /> : null
+      }
+
     </div>
   );
+}
+
+function Modal () {
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
 }
 
 export default App;
